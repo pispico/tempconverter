69eb5d4678345472d15ec10776e8afd5b4524102
williampispico <william.pispico@proposify.com>
2022-08-30 18:31:46 -0300

    adding conv.go

---
+++ conv.go
@@ -0,0 +1,7 @@
+package tempconverter
+
+//CToF converts Celsius temperature to Fahrenheit
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+//FToC converts Fahrenheit temperature to Celsius
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
